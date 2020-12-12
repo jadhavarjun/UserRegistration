993ca4b1ab26c96ef8f58b0ec5b7064604522ef6
arjunjava <jadhavarjun53@gmail.com>
2020-12-12 23:07:34 +0530

    Add : code to check First Name validation First latter much be capital and merge UC1 into master

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 
 echo "WELCOME TO USER REGISTRATION"
+read -p "Enter First Name : " firstName
+firstNamePattern="^[[:upper:]]{1}[[:lower:]]{2,}$"
+if [[ $firstName =~ $firstNamePattern ]]
+then
+	echo Valid
+else
+	echo Invalid
+fi
