ad28617067b8f899dbec86a313332c00a399740e
arjunjava <jadhavarjun53@gmail.com>
2020-12-13 14:41:58 +0530

    Add : code to added Rule3 should have atleast 1 numeric in the Password merge UC7 into master

--- userRegistration.sh
+++ userRegistration.sh
@@ -35,7 +35,7 @@ else
         echo Invalid
 fi
 read -p "Enter password : " password
-passwordPattern="^(.*[[:upper:]].*).{8}$"
+passwordPattern="^(.*[[:upper:]].*).{8}([0-9]{1})$"
 if [[ $password =~ $passwordPattern ]]
 then
         echo Valid
